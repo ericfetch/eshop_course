640b132a794c6fb98d4b9a85ee54ef7a30518113
eric <eric.fetech@gmail.com>
2025-02-20 14:31:28 +0800

    feat: useAppear

--- src/components/GoodsCard/index.js
+++ src/components/GoodsCard/index.js
@@ -1,47 +1,80 @@
-import React from'react';
+import React, { useEffect } from 'react';
 import './index.css';
 import goods01 from '../../assets/goods01.png';
-import  useNavigate  from '../../CRoute/useNavigate.js';
+import useNavigate from '../../CRoute/useNavigate.js';
+import useAppear from '../../Hooks/useAppear.js';
 export default function GoodsCard(props) {
   const { type = 1 } = props;
   const navigator = useNavigate();
+  const [ref, isAppear] = useAppear({
+    threshold: 0,
+    rootMargin: '40px',
+    once: true
+  });
   const hanldeToDetailPage = () => {
     // 跳轉到商品詳細頁面
     navigator('/detail');
   };
+  
+  useEffect(() => {
+    console.log('isAppear',isAppear);
+    
+  },[isAppear]);
 
-  return (
-    <>
-      {
-        type === 1 ? (
-          <div className="goods-card" onClick={hanldeToDetailPage}>
-            <div className="cover">
-              <img src={goods01} alt="" />
-            </div>
-            <div className='info'>
-              <div className="name">很長的商品名稱，也不知道要寫什麼但這樣夠長了再多就刪掉。</div>
-              <div className="sales">
-                                999,999 售出
-              </div>
-              <div className="price">
-                <span className='unit'>TWD</span>
-                <span className='value'>1,234,567</span>
-              </div>
-            </div>
+  const renderGoodsCard = () => {
+    if (type === 1) {
+      return (
+        <div className="goods-card" onClick={hanldeToDetailPage}>
+          <div className="cover">
+            <img src={goods01} alt="" />
           </div>
-        ) : (
-          <div className="goods-card-type2" onClick={hanldeToDetailPage}>
-            <div className="cover">
-              <img src={goods01} alt="" />
+          <div className='info'>
+            <div className="name">很長的商品名稱，也不知道要寫什麼但這樣夠長了再多就刪掉。</div>
+            <div className="sales">
+              999,999 售出
             </div>
-            <div className='info'>
-              <div className="name">很長的商品名稱，也不知道要寫什麼但這樣夠長了再多就刪掉。</div>
+            <div className="price">
               <span className='unit'>TWD</span>
               <span className='value'>1,234,567</span>
             </div>
           </div>
-        )
+        </div>
+      );
+    } else if (type === 2) {
+      return (
+        <div className="goods-card-type2" onClick={hanldeToDetailPage}>
+          <div className="cover">
+            <img src={goods01} alt="" />
+          </div>
+          <div className='info'>
+            <div className="name">很長的商品名稱，也不知道要寫什麼但這樣夠長了再多就刪掉。</div>
+            <span className='unit'>TWD</span>
+            <span className='value'>1,234,567</span>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="carrot-container">
+          <div className="image-wrapper">
+            <img
+              src="/images/carrots.jpg"
+              alt="一堆橙色胡萝卜"
+            />
+          </div>
+          <p className="carrot-text">
+            某個很長但名稱，被截斷...
+          </p>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div ref={ref}>
+      {
+        renderGoodsCard()
       }
-    </>
+    </div>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
-import './index.css'
-import CateSelect from "../../components/CateSelect";
-import Head from "../../components/Head";
+import React, { useEffect } from 'react';
+import './index.css';
+import CateSelect from '../../components/CateSelect';
+import Head from '../../components/Head';
 import BannerHomeSwiper from '../../components/BannerHomeSwiper';
 import BannerTwos from '../../components/BannerTwos';
 import UserLogin from '../../components/UserLogin';
@@ -9,35 +10,34 @@ import CartSuspend from '../../components/CartSuspend';
 
 export default function Home() {
 
-    return (
-        <>
-            <CartSuspend/>
-            <Head></Head>
-            <div className="home-content">
-                <div className="function-area">
-                    <CateSelect></CateSelect>
-                    <div className='mid-area'>
-                        <div className="banners-area">
-                            <BannerHomeSwiper></BannerHomeSwiper>
+  return (
+    <>
+      <CartSuspend/>
+      <Head></Head>
+      <div className="home-content">
+        <div className="function-area">
+          <CateSelect></CateSelect>
+          <div className='mid-area'>
+            <div className="banners-area">
+              <BannerHomeSwiper></BannerHomeSwiper>
 
-                        </div>
-                        <div className='banners-area'>
-                            <BannerTwos></BannerTwos>
-                            <div style={{ marginLeft: '20px' }}></div>
-                            <BannerTwos></BannerTwos>
-                        </div>
-                    </div>
-                    <UserLogin></UserLogin>
-                </div>
-                <div className='recommend-area'>
-                    <GoodsCard></GoodsCard>
-                    <GoodsCard></GoodsCard>
-                    <GoodsCard></GoodsCard>
-                    <GoodsCard></GoodsCard>
-                    <GoodsCard></GoodsCard>
-                    <GoodsCard></GoodsCard>
-                </div>
             </div>
-        </>
-    )
+            <div className='banners-area'>
+              <BannerTwos></BannerTwos>
+              <div style={{ marginLeft: '20px' }}></div>
+              <BannerTwos></BannerTwos>
+            </div>
+          </div>
+          <UserLogin></UserLogin>
+        </div>
+        <div className='recommend-area'>
+          {
+            new Array(20).fill(0).map((item,index) => {
+              return <GoodsCard key={index}></GoodsCard>;
+            })
+          }
+        </div>
+      </div>
+    </>
+  );
 }
